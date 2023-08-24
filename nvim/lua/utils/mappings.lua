55aeec3d9091c3fe4e8619279446cd4fa4703736
likalium <marcedeline@pm.me>
2023-08-24 14:25:29 +0200

    removed all these ~ files + unused files

--- nvim/lua/utils/mappings.lua
+++
@@ -1,7 +0,0 @@
--- Written from scratch by likalium :)
--- But thx so much to ThePrimegean's video: https://www.youtube.com/watch?v=w7i4amO_zaE
-
-vim.g.mapleader=","
-vim.keymap.set("n","<leader>s",vim.cmd.so)
-vim.keymap.set("n","<leader>c",vim.cmd.CHADopen)
-vim.keymap.set("n","<leader>ps",vim.cmd.PackerSync)
